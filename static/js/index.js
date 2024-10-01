439ff268ec553cdb4b6a397e3e009b4513c54404
RaffaeleeAuriole <aurioleraffaele@gmail.com>
2024-10-01 09:41:32 +0200

    fine
    funzione sanitize e commenti implementati al codice

--- static/js/index.js
+++ static/js/index.js
@@ -3,29 +3,46 @@
 }
 
 /*funzione per il sanitize l'input */
-function sanitize{
-
+function sanitize(input){
+    /*rimuove spazi bianchi econverte in  numero*/
+    return parseInt(input.trim(), 10);
 }
 
 /*creazione della costante del numero da 1 a 100 e la variabile che conta i tentativi */
 const numGen = genRandNum(1, 100);
 let tentativi = 5
 
+//inizio del gioco
 console.log("benvenuto al gioco 'indovina il numero'! hai 5 tentativi per indovinare il numero compreso tra 1 a 100 ");
 
+//inizio while
 while(tentativi > 0){
     let input = prompt("inserisci un numero da 1 a 100");
+
+    /*iniziamo il sanitize*/
+    let numUtente = sanitize(input);
+
+    //controllo dell'input dell'utente
+    if (isNaN(numUtente) || numUtente < 1 || numUtente > 100){
+        console.log("input non valido. inserisci un numero tra 1 e 100");
+    }
+
+    /*creazione dei vari segnali da dare all'utente per fargli indovinare il numero */
+    if(numUtente > numGen){
+        console.log("troppo grande");
+    }else if (numUtente < numGen){
+        console.log("troppo piccolo");
+    }else{
+        console.log("bravo, hai vinto!")
+    }
+
+    //decremento dei tentativi da 5 a 0
+    tentativi--;
+    console.log("ti restano: " + tentativi ,"tentativi");
+
+}//fine while
+
+//se l'utente esaurisce i tentativi
+if (tentativi === 0){
+    console.log("hai esaurito i tentativi, il numero corretto era: " + numGen);
 }
-
-/*iniziamo il sanitize*/
-let numUtente = sanitize(input);
-if (isNaN(numUtente) || numUtente < 1 || numUtente > 100){
-    console.log("input non valido. inserisci un numero tra 1 e 100");
-}
-
-/*creazione dei vari segnali da dare all'utente per fargli indovinare il numero */
-
-if(numUtente > numGen){
-    
-}
-
