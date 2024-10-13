6e43e59a3621593c8094d773d96a064b85cd1fac
RaffaeleeAuriole <aurioleraffaele@gmail.com>
2024-10-13 12:55:50 +0200

    correzione
    implementazione con html

--- static/js/index.js
+++ static/js/index.js
@@ -1,50 +1,35 @@
-/*funzione per generare il numero randomico da indovinare */function genRandNum(min,max){
-    return Math.floor(Math.random() * (max - min + 1))+ min;
-}
-
+/*funzione per generare il numero randomico da indovinare */
+function genRandNum(min = 0,  max = 100){
+            numGen = Math.random()
+            numGen = numGen * (max - min) + min
+            return Math.floor(numGen)
+        }
 /*funzione per il sanitize l'input */
 function sanitize(input){
-    /*rimuove spazi bianchi econverte in  numero*/
+    /*rimuove spazi bianchi e converte in  numero*/
     return parseInt(input.trim(), 10);
 }
-
-/*creazione della costante del numero da 1 a 100 e la variabile che conta i tentativi */
-const numGen = genRandNum(1, 100);
-let tentativi = 5
-
-//inizio del gioco
-console.log("---------------------------------------------------------------");
-console.log("benvenuto al gioco 'indovina il numero'!\nhai 5 tentativi per indovinare il numero compreso tra 1 a 100 ");
-console.log("---------------------------------------------------------------");
-
-//inizio while
-while(tentativi > 0){
-    let input = prompt("inserisci un numero da 1 a 100:  ");
-
-    /*iniziamo il sanitize*/
-    let numUtente = sanitize(input);
-
-    //controllo dell'input dell'utente
-    if (isNaN(numUtente) || numUtente < 1 || numUtente > 100){
-        console.log("input non valido. inserisci un numero tra 1 e 100:  ");
-    }
-
+//funzione che crea un evento e alla variabile numUtente fa il sanitize con la classe document usando il suo oggetto queryselector he seleziona l'attributo value
+function handleClick(event){
+    numUtente = sanitize(document.querySelector("#guess").value);
+    console.log(userNumber);
     /*creazione dei vari segnali da dare all'utente per fargli indovinare il numero */
     if(numUtente > numGen){
         console.log("troppo grande, riprova");
+        tentativi--;  
     }else if (numUtente < numGen){
         console.log("troppo piccolo, riprova");
+        tentativi--;
     }else{
         console.log("bravo, hai vinto!")
     }
-
-    //decremento dei tentativi da 5 a 0
-    tentativi--;
-    console.log("ti restano: " + tentativi ,"tentativi");
-
-}//fine while
-
-//se l'utente esaurisce i tentativi
-if (tentativi === 0){
-    console.log("hai esaurito i tentativi, il numero corretto era: " + numGen);
+    if (tentativi === 0){
+        console.log("hai esaurito i tentativi, il numero corretto era: " + numGen);
+    }
 }
+/*creazione della costante del numero da 1 a 100 e la variabile che conta i tentativi */
+const numGen = genRandNum(1, 100);
+let tentativi = 5
+console.log(randomNumber)
+let button = document.querySelector("#guessButton")
+button.addEventListener("click", handleClick);
