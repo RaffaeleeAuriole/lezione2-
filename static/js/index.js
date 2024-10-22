a679066caf286f6d8112d8d1135ac7cfc5c2729a
RaffaeleeAuriole <aurioleraffaele@gmail.com>
2024-10-22 10:47:27 +0200

    cambiamenti di alcuni errori sintattici

--- static/js/index.js
+++ static/js/index.js
@@ -1,58 +1,59 @@
-/*funzione per generare il numero randomico da indovinare */
-function genRandNum(min = 0,  max = 100){
-    let num = Math.floor(Math.random() * (max - min) + min);   
-    return num
+/* Funzione per generare il numero randomico da indovinare */
+function genRandNum(min = 0, max = 100) {
+    let num = Math.floor(Math.random() * (max - min) + min);
+    return num;
 }
 
-/*funzione per il sanitize l'input */
-function sanitize(Var){
-    let sanificata
-    if (!isNaN(Var)){
+/* Funzione per il sanitize dell'input */
+function sanitize(Var) {
+    let sanificata;
+    if (!isNaN(Var)) {
         sanificata = parseInt(Var.trim());
     } else {
         sanificata = NaN;
     }
-    return sanificata
+    return sanificata;
 }
 
-//funzione che crea un evento e alla variabile numUtente fa il sanitize con la classe document usando il suo oggetto queryselector he seleziona l'attributo value
-function handleClick(event){
+/* Funzione che crea un evento e alla variabile numUtente fa il sanitize con la classe document usando il suo oggetto queryselector he seleziona l'attributo value */
+function handleClick(event) {
     let input = document.querySelector("#guess").value;
-    let numeroInserito= sanitize(input);
+    let numeroInserito = sanitize(input);
     let ele1 = document.querySelector("#messaggio1");
     let ele2 = document.querySelector("#messaggio2");
 
-    if(isNaN(numeroInserito)) {
-        ele1.innerHTML= `non hai ancora inserito nessun numero`;
-        ele2.innerHTML=`inserisci un numero valido`;
+    if (isNaN(numeroInserito)) {
+        ele1.innerHTML = "Non hai ancora inserito nessun numero";
+        ele2.innerHTML = "Inserisci un numero valido";
     } else {
-        if(numeroInserito > numGenerato){
-            ele1.innerHTML("il numero <strong>" + numeroInserito + "</strong> è troppo grande");
+        if (numeroInserito > numGenerato) {
+            ele1.innerHTML = "Il numero <strong>" + numeroInserito + "</strong> è troppo grande";
             tentativi -= 1;
             index += 1;
-            ele2.innerHTML = "hai ancora a disposizione <strong>" + tentativi + "</strong> tentativi";
-        }else if(numeroInserito < numGenerato){
-            ele1.innerHTML="il numero <strong>" + numeroInserito + "</strong> è  troppo piccolo"
+            ele2.innerHTML = "Hai ancora a disposizione <strong>" + tentativi + "</strong> tentativi";
+        } else if (numeroInserito < numGenerato) {
+            ele1.innerHTML = "Il numero <strong>" + numeroInserito + "</strong> è troppo piccolo";
             tentativi -= 1;
             index += 1;
-            ele2.innerHTML= "hai ancora a disposizione<strong>" + tentativi + "</strong> tentativi";
-        }else {
-            ele1.innerHTML = "bravo, hai vinto! hai usato <strong>" + (index + 1) + "</strong> tentativi";
-            ele2.innerHTML = "il numero è effettivamente <strong>" + numeroInserito + "</strong>";
+            ele2.innerHTML = "Hai ancora a disposizione <strong>" + tentativi + "</strong> tentativi";
+        } else {
+            ele1.innerHTML = "Bravo, hai vinto! Hai usato <strong>" + (index + 1) + "</strong> tentativi";
+            ele2.innerHTML = "Il numero è effettivamente <strong>" + numeroInserito + "</strong>";
             indovinato = true;
             document.querySelector("#guess").disabled = true;
             document.querySelector("#guessBtn").disabled = true;
         }
     }
 
-    if (tentativi === 0 && !indovinato){
-        ele1.innerHTML = "mi dispiace, hai perso.";
-        ele2.innerHTML = "il numero era <strong>" + numGenerato + "</strong>";
+    if (tentativi === 0 && !indovinato) {
+        ele1.innerHTML = "Mi dispiace, hai perso.";
+        ele2.innerHTML = "Il numero era <strong>" + numGenerato + "</strong>";
         document.querySelector("#guess").disabled = true;
         document.querySelector("#guessBtn").disabled = true;
     }
 }
 
+/* Creazione della costante del numero da 1 a 100 e la variabile che conta i tentativi */
 const numGenerato = genRandNum(1, 100);
 let tentativi = 5;
 let index = 0;
